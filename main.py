0b37e80959ab8f7a44dc026ce5f32d907a444b80
sumit pandit <its.sumitpandit@gmail.com>
2026-07-25 19:07:37 +0530

    cors updated

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ app = FastAPI()
 # Enable CORS for external frontend applications
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://novamind-beta.vercel.app/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
